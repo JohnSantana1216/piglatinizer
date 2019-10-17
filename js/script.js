c7d277087e0dc1872084f8442dc607e772eb20e3
JohnSantana1216 <john.santana@uasdc.org>
2019-10-17 22:18:19 +0000

    oct17

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+    let input= $("#input").val();
+var firstcharacter = input.charAt(0);
+var word = ""
+});
 
 
 
-  
-  
-  
-  
+
+
+
 });
