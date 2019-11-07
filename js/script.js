5faadc30576064318444909b8d4e7af62902fc75
JohnSantana1216 <john.santana@uasdc.org>
2019-11-07 23:19:20 +0000

    Added click handler and array

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-    let input= $("#input").val();
-var firstcharacter = input.charAt(0);
-var word = ""
+    let input= $(".input").val();
+    var firstcharacter = input.charAt(0);
+
+    $("#button").click(function() {
+        console.log ("11");
+        $("output").text("Hello");
+    });
 });
+var order= []
 
-
-
-
-
-
+$("#button").click(function() {
+    console.log ("11");
+    $("output").text("Bye");
 });
